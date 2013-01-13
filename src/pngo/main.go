33f56db462da703c3dba884578eb96ccded059e2
Jay Weisskopf <jay@jayschwa.net>
2013-01-13 04:11:12 -0600

    Put convert code in separate function
    Prevent "too many open files" error.

--- src/pngo/main.go
+++ src/pngo/main.go
@@ -16,49 +16,53 @@ import (
 	"strings"
 )
 
+func convert(name string) {
+	fmt.Print(name, " -> ")
+
+	// Open input image file for reading
+	fin, err := os.Open(name)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer fin.Close()
+
+	// Decode image data
+	img, _, err := image.Decode(fin)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Change extension to .png
+	ext := path.Ext(name)
+	if len(ext) > 0 {
+		name = strings.Replace(name, ext, "", -1)
+	}
+	name += ".png"
+
+	// Create output file for writing
+	fout, err := os.Create(name)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func() {
+		err := fout.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	// Write PNG data to output file
+	err = png.Encode(fout, img)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(name)
+}
+
 func main() {
 	flag.Parse()
-	for _, name := range flag.Args() {
-		fmt.Print(name, " -> ")
-
-		// Open input image file for reading
-		fin, err := os.Open(name)
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer fin.Close()
-
-		// Decode image data
-		img, _, err := image.Decode(fin)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		// Change extension to .png
-		ext := path.Ext(name)
-		if len(ext) > 0 {
-			name = strings.Replace(name, ext, "", -1)
-		}
-		name += ".png"
-
-		// Create output file for writing
-		fout, err := os.Create(name)
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer func() {
-			err := fout.Close()
-			if err != nil {
-				log.Fatal(err)
-			}
-		}()
-
-		// Write PNG data to output file
-		err = png.Encode(fout, img)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		fmt.Println(name)
+	for _, f := range flag.Args() {
+		convert(f)
 	}
 }
